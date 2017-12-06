e98e8b1a06fd3af75fe5353d4d388eebb4de2b64
grzegorz jaczewski <grzesiek.jaczewski@gmail.com>
2017-12-06 09:55:39 +0100

    Zmana w widoku i kontrolerze Asset

--- FixedAssets/FixedAssets/Controllers/AssetsController.cs
+++ FixedAssets/FixedAssets/Controllers/AssetsController.cs
@@ -46,7 +46,7 @@ namespace FixedAssets.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,AssetName,IsUsed")] Asset asset)
+        public ActionResult Create([Bind(Include = "Id,AssetName,Quantity,StartUsingDate,InitialValue,Depreciationeted,IsUsed")] Asset asset)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +78,7 @@ namespace FixedAssets.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,AssetName,IsUsed")] Asset asset)
+        public ActionResult Edit([Bind(Include = "Id,AssetName,Quantity,StartUsingDate,InitialValue,Depreciationeted,IsUsed")] Asset asset)
         {
             if (ModelState.IsValid)
             {
