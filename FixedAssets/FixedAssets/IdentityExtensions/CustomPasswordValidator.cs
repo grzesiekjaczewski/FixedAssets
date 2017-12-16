b978f684292870b9e3ce16b7b6637366008e3057
grzegorz jaczewski <grzesiek.jaczewski@gmail.com>
2017-12-16 14:30:13 +0100

    CustomPasswordValidator

---
+++ FixedAssets/FixedAssets/IdentityExtensions/CustomPasswordValidator.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FixedAssets.IdentityExtensions
+{
+    public class CustomPasswordValidator : PasswordValidator, IIdentityValidator<string>
+    {
+        override public Task<IdentityResult> ValidateAsync(string item)
+        {
+            if (String.IsNullOrEmpty(item) || item.Length < RequiredLength)
+            {
+                return Task.FromResult(IdentityResult.Failed(String.Format("Password should be of length {0}", RequiredLength)));
+            }
+
+            string pattern = @"^(?=.*[0-9])(?=.*[!@#$%^&*])[0-9a-zA-Z!@#$%^&*0-9]{10,}$";
+
+            if (!Regex.IsMatch(item, pattern))
+            {
+                return Task.FromResult(IdentityResult.Failed("Dupa Blada!!!, Passwords must have at least one non letter or digit character. Passwords must have at least one digit ('0'-'9'). Passwords must have at least one uppercase ('A'-'Z')"));
+            }
+
+            return Task.FromResult(IdentityResult.Success);
+        }
+    }
+}
